2ad653f75687c24d052363c5c64f35c9cf080cc5
Tanuki <80181268+irukaSaka@users.noreply.github.com>
2022-07-24 09:56:58 +0900

    emotion

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,7 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
-
+import styled from "@emotion/styled";
 export const Emotion = () => {
   const containerStyle = css`
     border: solid 2px #392eff;
@@ -12,10 +12,20 @@ export const Emotion = () => {
     justify-content: space-around;
     align-items: center;
   `;
+  const titleStyle = css({
+    margin: "0px",
+    color: "#3d84a8"
+  });
   return (
     <div css={containerStyle}>
-      <p>- Emotion -</p>
-      <button>FIGHT!!</button>
+      <p css={titleStyle}>- Emotion -</p>
+      <SButton>FIGHT!!</SButton>
     </div>
   );
 };
+const SButton = styled.button`
+  background-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+`;
